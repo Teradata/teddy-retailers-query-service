6ab22b16a255f5ebacd1bedad51420f3b27227b0
vidhanbhonsle <vidhanbhonsle@gmail.com>
2023-06-29 18:46:56 +0530

    edited python code

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
-const load_data = () => {
-    fetch('/static/mock_data/orders.json')
+const get_customer_data = (customer_id) => {
+    fetch('/orders/' + customer_id)
     .then(response => response.json())
     .then(data => {
         // Now you can access the orders data
-        mock_data = data;
+        console.log(data);
+        return data
+        // console.log(mock_data);
         // Do further processing with the data here
     })
     .catch(error => {
@@ -12,43 +14,40 @@ const load_data = () => {
     });
 }
 
-const get_customer_data = (customer_id) => {
-    //make request to get customer data to backend
-    console.log() //the customer data
-}
-
 const get_customer_discount = () => {
     //make request to get customer discount to backend
     console.log()
 }
 
-const populate_customer = (customer_id) => {
-    const selected_customer_data = mock_data.orders.find((order) => {return order.customer === customer});
-    const table_orders = document.querySelector("#orderTable");
-    table_orders.innerHTML = "";
-    const heading_row = document.createElement("tr");
-    heading_row.innerHTML = `
-        <th>Product Code</th>
-        <th>Product Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-    `;
-    table_orders.appendChild(heading_row)
-    selected_customer_data.lines.forEach((line) => {
-        const newRow = document.createElement("tr");
-        newRow.innerHTML = `
-          <td>${line.product_code}</td>
-          <td>${line.product_name}</td>
-          <td>${line.quantity}</td>
-          <td>$${(line.subtotal / 100).toFixed(2)}</td>
-        `;
-        table_orders.appendChild(newRow);
-      });
+const populate_customer = async (customer_id) => {
+    // console.log(customer_id);
+    const selected_customer_data = await get_customer_data(customer_id);
+
+    // const table_orders = document.querySelector("#orderTable");
+    // table_orders.innerHTML = "";
+    // const heading_row = document.createElement("tr");
+    // heading_row.innerHTML = `
+    //     <th>Product Code</th>
+    //     <th>Product Name</th>
+    //     <th>Quantity</th>
+    //     <th>Price</th>
+    // `;
+    // table_orders.appendChild(heading_row)
+    // selected_customer_data.lines.forEach((line) => {
+    //     const newRow = document.createElement("tr");
+    //     newRow.innerHTML = `
+    //       <td>${line.product_code}</td>
+    //       <td>${line.product_name}</td>
+    //       <td>${line.quantity}</td>
+    //       <td>$${(line.subtotal / 100).toFixed(2)}</td>
+    //     `;
+    //     table_orders.appendChild(newRow);
+    //   });
 }
 
-let mock_data = "";
-load_data();
-console.log(mock_data);
+// let mock_data = "";
+// load_data();
+// console.log(mock_data);
 
 
 
--- teddy_qs.py
+++ teddy_qs.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, jsonify, make_response
+from flask import Flask, render_template, request, redirect, jsonify, make_response, Response
+from os import path as op
+import json
 
 app = Flask(__name__)
 
@@ -6,17 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-'''
-@app.route("/orders", methods=["GET"])
-def get_data():
-    req = request.get_json()
-     ### Read json file
-    response = json.loads(json.file)[request["id"]]
-    res = make_response(json.dumps(response), 200)
-    return res
 
-@app.route("/discount", methods=["GET"])
-def get_discount():
-    #calls query service
-'''
-# Hi!
+@app.route("/orders/<string:customer_id>", methods=["GET"])
+def get_data(customer_id):
+    fileName = op.join(op.realpath(op.dirname(__file__)), 'mock_data/orders.json')
+    data = json.load(open(fileName))
+    for order in data['orders']:
+        if((customer_id == order['customer'])):
+            customer_orders = order['lines']
+            break
+        else:
+            print("No record found")
+    return Response(response=json.dumps(customer_orders),status=200,mimetype='application/json')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
