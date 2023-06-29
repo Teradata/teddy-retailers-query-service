072ccab7ffb7bfed6adf927a34fed048d69104c0
Daniel Herrera <daniel.herrera2@teradata.com>
2023-06-29 15:31:40 +0200

    half of the way

--- db_service.py
+++ db_service.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
 import base64
+import json
+import requests
+import time
+from datetime import datetime, timedelta
 
 load_dotenv()
-host = os.getenv("TD_HOST")
-user = os.getenv("TD_USER")
-password = os.getenv("TD_PASSWORD")
+
+host = os.environ.get("TD_HOST")
+user = os.environ.get("TD_USER")
+password = os.environ.get("TD_PASSWORD")
 query_endpoint = f'https://{host}:1443/systems/local/queries'
 
 auth_encoded = user + ':' + password
@@ -17,12 +22,19 @@ headers = {
   'Authorization': auth_str # base 64 encoded username:password
 }
 
-def get_user_tlv():
-    '''    
+def get_user_tlv(customer_id):
+    query = f'SELECT customer_id,tlv,last_ordered FROM teddy_retailers_warehouse.customers_tlv WHERE customer_id={customer_id};'    
     payload = {
-      'query': example_query, # 'SELECT * FROM DBC.DBCInfo;',
+      'query': query,
       'format': 'OBJECT',
       'includeColumns': True,
-      'rowLimit': 4
     }
-    '''
+    payload_json = json.dumps(payload)
+    response = requests.request('POST', query_endpoint, headers=headers, data=payload_json, verify=False)
+    return response.json()
+
+def check_time(eval_unix_timestamp):
+  current_timestamp = time.time()
+  current_datetime = datetime.fromtimestamp(current_timestamp)
+  target_datetime = current_datetime - timedelta(days=200)
+  return current_datetime <= datetime.fromtimestamp(eval_unix_timestamp) <= target_datetime
--- teddy_qs.py
+++ teddy_qs.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, make_response, Response
 from os import path as op
 import json
+from db_service import get_user_tlv
 
 app = Flask(__name__)
 
@@ -13,27 +14,16 @@ def get_data():
     fileName = op.join(op.realpath(op.dirname(__file__)), 'mock_data/orders.json')
     data = json.load(open(fileName))
     return Response(response=json.dumps(data),status=200,mimetype='application/json')
-
-'''
-@app.route("/orders", methods=["GET"])
-def get_data():
-    req = request.get_json()
-     ### Read json file
-    response = json.loads(json.file)[request["id"]]
-    res = make_response(json.dumps(response), 200)
-    return res
-'''
     
-@app.route("/discount", methods=["GET"])
-def get_discount():
-    req = request.get_json()
-    fileName = op.join(op.realpath(op.dirname(__file__)), 'mock_data/orders.json')
-    data = json.load(open(fileName))
-
-
+@app.route("/customer_tlv/<string:customer_id>", methods=["GET"])
+def get_discount(customer_id):
+    tlv_data = get_user_tlv(customer_id)
+    data_to_process = tlv_data["results"][0]["data"][0]
+    print(data_to_process)
+    response = make_response(json.dumps(tlv_data["results"][0]["data"]), 200)
+    return response
+    
 @app.route('/orders/<string:customer_id>') 
 def get_orders(customer_id):
-    path = 
-    data = js
     return f"<p>{customer_id}</p>"
 
