4a66d6d92622add882952a14f57edb586332500f
Daniel Herrera <daniel.herrera2@teradata.com>
2023-06-29 16:38:07 +0200

    completed endpoint in python for discount

--- db_service.py
+++ db_service.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 import base64
 import json
 import requests
+import time
+from datetime import datetime, timedelta
+
 
 load_dotenv()
 
@@ -29,5 +32,25 @@ def get_user_tlv(customer_id):
     }
     payload_json = json.dumps(payload)
     response = requests.request('POST', query_endpoint, headers=headers, data=payload_json, verify=False)
-    return response.json()
+    data_to_process = response.json()
+    data_to_process = data_to_process["results"][0]["data"][0]
+    data_to_process["discount"] = get_discount(data_to_process["tlv"],data_to_process["last_ordered"])
+    return data_to_process
 
+def get_discount(tlv,unix_timestamp_last_order):
+    print(is_current_customer(unix_timestamp_last_order))
+    if is_current_customer(unix_timestamp_last_order):
+        if tlv >= 1500:
+            return 10
+        elif tlv >= 1000:
+            return 5
+        else:
+            return 0
+    else:
+        return 0
+
+
+def is_current_customer(eval_unix_timestamp):
+  print(type(eval_unix_timestamp))
+  current_timestamp = int(time.time())
+  return (current_timestamp - eval_unix_timestamp) <= 17280000
--- teddy_qs.py
+++ teddy_qs.py
@@ -2,38 +2,32 @@ from flask import Flask, render_template, request, redirect, jsonify, make_respo
 from os import path as op
 import json
 from db_service import get_user_tlv
-import time
-from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
-@app.route("/orders", methods=["GET"])
-def get_data():
+    
+@app.route("/orders/<string:customer_id>", methods=["GET"])
+def get_data(customer_id):
     fileName = op.join(op.realpath(op.dirname(__file__)), 'mock_data/orders.json')
     data = json.load(open(fileName))
-    return Response(response=json.dumps(data),status=200,mimetype='application/json')
+    for order in data['orders']:
+        if((customer_id == order['customer'])):
+            customer_orders = order['lines']
+            break
+        else:
+            print("No record found")
+    return Response(response=json.dumps(customer_orders),status=200,mimetype='application/json')
     
 @app.route("/customer_tlv/<string:customer_id>", methods=["GET"])
-def get_discount(customer_id):
+def get_customer_data(customer_id):
     tlv_data = get_user_tlv(customer_id)
-    try:
-        data_to_process = tlv_data["results"][0]["data"][0]
-        #data_to_process["discount"] = get_discount(data_to_process["tlv"],data_to_process["last_ordered"])
-        response = make_response(json.dumps(data_to_process), 200)
-    except Exception as e:
-        print("An error ocurrer:", e)
-  
+    response = make_response(tlv_data, 200)
+    return response
     
 @app.route('/orders/<string:customer_id>') 
 def get_orders(customer_id):
     return f"<p>{customer_id}</p>"
 
-def is_current_customer(eval_unix_timestamp):
-  current_timestamp = time.time()
-  current_datetime = datetime.fromtimestamp(current_timestamp)
-  target_datetime = current_datetime - timedelta(days=200)
-  return current_datetime <= datetime.fromtimestamp(eval_unix_timestamp) <= target_datetime
