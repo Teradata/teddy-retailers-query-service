a01b084044bbf1dce64c7a4732e567d9aca1e494
Daniel Herrera <daniel.herrera2@teradata.com>
2023-06-29 22:58:50 +0200

    completed demo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Query Service Demo
+# Introduction
+## About Teradata Query Service
+1. What is Query Service
+2. Use cases
+## About the project
+# Prerequisites
+# Steps
+1. Get clearscape account
+2. Write credentials to .env file.
+3. Uploading data
+4. Confirm the data
+5. Set python
+    venv
+    pip install -r requirements.txt
+6. Run the project
--- static/js/script.js
+++ static/js/script.js
@@ -1,51 +1,61 @@
-const get_customer_data = async (customer_id) => {
-    fetch('/orders/' + customer_id)
-    .then(response => response.json())
-    .then(data => {
-        // Now you can access the orders data
-        console.log(data);
-        return data
-        // Do further processing with the data here
-    })
-    .catch(error => {
-        // Handle any errors that occurred during the fetch
-        console.error('Error:', error);
-    });
+const get_customer_data = async (endpoint,customer_id) => {
+    try {
+      const response = await fetch(`/${endpoint}/${customer_id}`);
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+        }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      // Handle the error here
+      console.error('Error fetching customer data:', error);
+    }
+  };
+
+const process_customer_orders = async (customer_id) => {
+    const customer_orders = await get_customer_data("orders",customer_id);
+    const customer_tlv = await get_customer_data("customer_tlv",customer_id);
+    const orders_subtotal = customer_orders.reduce((total, product) => total + product.subtotal, 0);
+    const discount = (orders_subtotal * customer_tlv["discount"]) / 100
+    const total = orders_subtotal - discount
+    populate_customer(customer_orders);
+    populate_totals (orders_subtotal,discount,total);
 }
 
-const get_customer_discount = () => {
-    //make request to get customer discount to backend
-    console.log()
+const populate_customer = (selected_customer_data) => {
+     const table_orders = document.querySelector("#orderTable");
+     table_orders.innerHTML = "";
+     const heading_row = document.createElement("tr");
+     heading_row.innerHTML = `
+         <th>Product Code</th>
+         <th>Product Name</th>
+         <th>Quantity</th>
+         <th>Price</th>
+     `;
+     table_orders.appendChild(heading_row)
+     selected_customer_data.forEach((line) => {
+         const newRow = document.createElement("tr");
+         newRow.innerHTML = `
+           <td>${line.product_code}</td>
+           <td>${line.product_name}</td>
+           <td>${line.quantity}</td>
+           <td>$${(line.subtotal / 100).toFixed(2)}</td>
+         `;
+         table_orders.appendChild(newRow);
+       });
 }
 
-const populate_customer = (customer_id) => {
-    // const selected_customer_data = mock_data.orders.find((order) => {return order.customer === customer});
-    const selected_customer_data = get_customer_data(customer_id);
-    // const table_orders = document.querySelector("#orderTable");
-    // table_orders.innerHTML = "";
-    // const heading_row = document.createElement("tr");
-    // heading_row.innerHTML = `
-    //     <th>Product Code</th>
-    //     <th>Product Name</th>
-    //     <th>Quantity</th>
-    //     <th>Price</th>
-    // `;
-    // table_orders.appendChild(heading_row)
-    // selected_customer_data.lines.forEach((line) => {
-    //     const newRow = document.createElement("tr");
-    //     newRow.innerHTML = `
-    //       <td>${line.product_code}</td>
-    //       <td>${line.product_name}</td>
-    //       <td>${line.quantity}</td>
-    //       <td>$${(line.subtotal / 100).toFixed(2)}</td>
-    //     `;
-    //     table_orders.appendChild(newRow);
-    //   });
+const populate_totals = (sub_total, discount, total) => {
+    const totals_detail = document.querySelector("#totals_detail");
+    totals_detail.innerHTML = ""
+    totals_detail.innerHTML = `
+    <h4 >Total Order $${(sub_total / 100).toFixed(2)} </h4>
+    <h4 >Discount $${(discount / 100).toFixed(2)} </h4>
+    <hr>
+    <h4 >To Pay $${(total / 100).toFixed(2)} </h4>
+    <button class="btn btn-secondary text-right"> Pay Now </button>
+    `
 }
-
-// let mock_data = "";
-// load_data();
-// console.log(mock_data);
 
 
 
