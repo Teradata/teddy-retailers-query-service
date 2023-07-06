006467e915fcc98ca4eb89039404513f2c2ef2db
Daniel Herrera <daniel.herrera2@teradata.com>
2023-07-06 17:46:05 +0200

    corrected data ingestion

--- mock_data/data_load.sql
+++ mock_data/data_load.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE teddy_retailers_warehouse
 AS PERMANENT = 110e6;
-CREATE TABLE teddy_retailers_warehouse.customers_tvl AS
+CREATE TABLE teddy_retailers_warehouse.customers_tlv AS
 	(
 		SELECT CAST(customer_id as CHAR(5)) as customer_id, email, bought_items, tlv, last_ordered  FROM 
 		(
